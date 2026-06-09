3abe21ffb314a83f0dabb286214216566660d388
Alain Clément <alain.clement@unil.ch>
2026-06-09 16:59:24 +0200

    chore: bump rstudio-cli to 0.19.1

--- Formula/rstudio-cli.rb
+++ Formula/rstudio-cli.rb
@@ -1,28 +1,28 @@
 class RstudioCli < Formula
   desc "AI-native CLI bridge to drive an RStudio Server/Desktop IDE from a terminal"
   homepage "https://github.com/aclemen1/rstudio-cli"
-  version "0.19.0"
+  version "0.19.1"
   license "MIT"
 
   on_macos do
     on_arm do
-      url "https://github.com/aclemen1/rstudio-cli/releases/download/v0.19.0/rstudio-cli-v0.19.0-aarch64-apple-darwin.tar.gz"
-      sha256 "8af564aa6d8874eb28d9fd23753f4be0d5031b1b478a604e110927362ee27a99"
+      url "https://github.com/aclemen1/rstudio-cli/releases/download/v0.19.1/rstudio-cli-v0.19.1-aarch64-apple-darwin.tar.gz"
+      sha256 "ab51f2436c274086ed489dfd90a9becfbef407715471312e8a96465c342eeb48"
     end
     on_intel do
-      url "https://github.com/aclemen1/rstudio-cli/releases/download/v0.19.0/rstudio-cli-v0.19.0-x86_64-apple-darwin.tar.gz"
-      sha256 "5ec9b74333c7f8aad9cd9ab6fecbe9ec6a6db3a9942d632de5b346d3e11b1b47"
+      url "https://github.com/aclemen1/rstudio-cli/releases/download/v0.19.1/rstudio-cli-v0.19.1-x86_64-apple-darwin.tar.gz"
+      sha256 "214b3b65f12cad585b7a2b52b174bad38fff04dddbde388da052e6ccbf962560"
     end
   end
 
   on_linux do
     on_arm do
-      url "https://github.com/aclemen1/rstudio-cli/releases/download/v0.19.0/rstudio-cli-v0.19.0-aarch64-unknown-linux-gnu.tar.gz"
-      sha256 "a8178fc1857bfa028175bc70a79a244f46566dffcc674dba2f810e244d1bc042"
+      url "https://github.com/aclemen1/rstudio-cli/releases/download/v0.19.1/rstudio-cli-v0.19.1-aarch64-unknown-linux-gnu.tar.gz"
+      sha256 "ae6d684dbdd66dd01ad0fefed3d17cb183489436ef7f7e9fb6bead319e275076"
     end
     on_intel do
-      url "https://github.com/aclemen1/rstudio-cli/releases/download/v0.19.0/rstudio-cli-v0.19.0-x86_64-unknown-linux-gnu.tar.gz"
-      sha256 "0171ccd8c903b55e7aeb9b986185671cadf71f488a4f4090dc54a8553dd80706"
+      url "https://github.com/aclemen1/rstudio-cli/releases/download/v0.19.1/rstudio-cli-v0.19.1-x86_64-unknown-linux-gnu.tar.gz"
+      sha256 "154b927711e89a23bfe4c416fa3dcf919d23191638216a7ac40ff0dd9d1eacb0"
     end
   end
 
@@ -31,6 +31,6 @@ class RstudioCli < Formula
   end
 
   test do
-    assert_match "0.19.0", shell_output("#{bin}/rstudio version")
+    assert_match "0.19.1", shell_output("#{bin}/rstudio version")
   end
 end
